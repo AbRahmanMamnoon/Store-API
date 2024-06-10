65e45fce24ab4763fb53eca6d43dc6979ff3d734
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-06-10 21:07:50 +0430

    The pagination, limit, and skip features added

--- controllers/productController.js
+++ controllers/productController.js
@@ -45,6 +45,13 @@ exports.getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const product = await result;
 
   res.status(200).json({
