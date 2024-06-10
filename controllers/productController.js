436fae6ef5a952cacbbc527953f91da3bb0d6dd7
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-06-10 20:16:32 +0430

    added searching, filtering, sorting and selecting special some fields.

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,13 +1,57 @@
+const Product = require('../models/products');
 
-exports.getAllProductsStatic = (req, res) => {
-    throw new Error('Testing express-async-errors');
-    res.status(200).json({
-        msg: 'Testing the product'
-    });
+exports.getAllProductsStatic = async (req, res) => {
+  const search = 'e';
+  const product = await Product.find()
+    .sort('-price')
+    .limit(10)
+    .skip(1)
+    .select('company rating');
+    
+  res.status(200).json({
+    status: 'success',
+    length: product.length,
+    data: {
+      product
+    }
+  });
 }
 
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({
-        msg: 'All products...'
-    });
+exports.getAllProducts = async (req, res) => {
+
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if(featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if(company) {
+    queryObject.company = company;
+  }
+  if(name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+  // console.log(queryObject);
+
+  let result = Product.find(queryObject);
+  // sort
+  if(sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort(createdAt);
+  }
+  if(fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+  const product = await result;
+
+  res.status(200).json({
+    status: 'success',  
+    Length: product.length,
+    data: {
+      product,
+    }
+  });
 }
