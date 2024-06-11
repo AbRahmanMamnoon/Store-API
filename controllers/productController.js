dd363b01bc6c954e3c1be950dd5ef631c4e6615b
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-06-11 20:50:31 +0430

    added numeric filter on the fields like price, rating.

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,7 +7,7 @@ exports.getAllProductsStatic = async (req, res) => {
     .limit(10)
     .skip(1)
     .select('company rating');
-    
+
   res.status(200).json({
     status: 'success',
     length: product.length,
@@ -19,29 +19,46 @@ exports.getAllProductsStatic = async (req, res) => {
 
 exports.getAllProducts = async (req, res) => {
 
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
-  if(featured) {
+  if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
-  if(company) {
+  if (company) {
     queryObject.company = company;
   }
-  if(name) {
+  if (name) {
     queryObject.name = { $regex: name, $options: 'i' }
   }
-  // console.log(queryObject);
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+    const options = ['price', 'rating'];
+    filters = filters.split(',').forEach(item => {
+      const [field, operator, value] = item.split('-');
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
 
   let result = Product.find(queryObject);
   // sort
-  if(sort) {
+  if (sort) {
     const sortList = sort.split(',').join(' ');
     result = result.sort(sortList);
   } else {
-    result = result.sort(createdAt);
+    result = result.sort(Product.createdAt);
   }
-  if(fields) {
+  if (fields) {
     const fieldsList = fields.split(',').join(' ');
     result = result.select(fieldsList);
   }
@@ -55,7 +72,7 @@ exports.getAllProducts = async (req, res) => {
   const product = await result;
 
   res.status(200).json({
-    status: 'success',  
+    status: 'success',
     Length: product.length,
     data: {
       product,
